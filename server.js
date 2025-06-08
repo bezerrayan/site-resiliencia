b9138bdc4a10de9248590c5885c04633cae9559a
yan Bezerra <bezerrayan651@gmail.com>
2025-06-08 17:43:35 -0300

    atualização local

--- server.js
+++ server.js
@@ -55,4 +55,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () =>
+  console.log(`Servidor rodando em http://0.0.0.0:${PORT}`)
+);
